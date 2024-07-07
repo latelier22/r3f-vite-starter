c73510976310bf0be560d1fd14623df20300a750
latelier22 <latelier.cyrille@gmail.com>
2024-07-07 19:16:43 +0200

    fond blanc bleu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <Canvas shadows camera={{ position: [-10, 4, 15], fov: 30 }}>
-      <color attach="background" args={["#000022"]} />
+      <color attach="background" args={["#EEEEFF"]} />
       <Experience />
     </Canvas>
   );
